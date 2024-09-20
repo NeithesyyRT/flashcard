fc994f52b8a2d0ff63557e3ae477e914a0007f85
Nathan <140825380+NeithesyyRT@users.noreply.github.com>
2024-09-20 09:41:45 -0300

    z

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function criaCartao(categoria, pergunta, resposta) {
 
     cartao.innerHTML = `
     <div class="cartao__conteudo">
-    <h3?${categoria}</h3>
-     <div class="cartao__conteudo__pergunta">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
